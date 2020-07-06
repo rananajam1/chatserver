7e23f36c6f32af51f02f5a411637c7767a8273e4
Rana Najam <rnajam147@icloud.com>
2020-07-06 21:26:11 +0500

    Add sent messages and update read endpoints

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,6 @@ api.post("/MakeConnection", (req, res) => {
   var today = new Date();
   var time =
     today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  console.log(dbConfig);
   var Query =
     "if not exists(select * from[cmatrix_connections] where[ccon_connection_id] = @ConnectionID)";
   Query += " begin ";
@@ -44,6 +43,78 @@ api.post("/MakeConnection", (req, res) => {
   });
 });
 
+api.post("/broadcast", function (req, res) {
+  console.log(req.body.UserObj);
+  let buff = new Buffer.from(req.body.familyCode, "base64");
+  let text = buff.toString("utf8");
+  let splitColons = text.split(";");
+  let username = splitColons[1].split("=");
+  let password = splitColons[2].split("=");
+  let database = splitColons[3].split(" = ");
+  var dbConfig = {
+    user: username[1],
+    password: password[1],
+    server: "58.65.129.101",
+    database: database[1],
+  };
+  console.log(database[1])
+  var today = new Date();
+  today = today.toISOString().replace('T',' ').replace('Z','')
+    console.log(today)
+  var Query =
+    "insert into cmatrix_received_sms (crsms_cmp_key,crsms_is_sms,crsms_to_user_name,crsms_is_read, crsms_body, crsms_text_body, crsms_from_user_key, crsms_from_user_name, crsms_to_user_key,crsms_created_date) values(@CompanyID,@isSMS,@ToUserName,@isRead,@Message,@Message,@FromID,@FromUserName,@ToUserID,@ConnectionTime)";
+  var connection = new sql.ConnectionPool(dbConfig, function (err) {
+    var r = new sql.Request(connection);
+    r.input("CompanyID", sql.VarChar, req.body.UserObj.CompanyID),
+      r.input("isSMS", sql.VarChar, false),
+      r.input("ToUserName", sql.VarChar, req.body.UserObj.ToUsername),
+      r.input("ToUserID", sql.VarChar, req.body.UserObj.ToId),
+      r.input("FromUserName", sql.VarChar, req.body.UserObj.FromUserName),
+      r.input("FromID", sql.VarChar, req.body.UserObj.FromId),
+      r.input("isRead", sql.VarChar, req.body.UserObj.isRead),
+      r.input("Message", sql.VarChar, req.body.UserObj.Message),
+      r.input("ConnectionTime", sql.VarChar, today);
+    r.multiple = true;
+    r.query(Query, function (err, recordsets) {
+      connection.close();
+      if (err) console.error(err);
+      else console.dir("SUCCESS", recordsets);
+    });
+  });
+});
+
+api.get('/UpdateMessageReadStatus/:From/:To/:familyCode',function(req,res){
+  console.log(req.params)
+  let buff = new Buffer.from(req.params.familyCode, "base64");
+  let text = buff.toString("utf8");
+  let splitColons = text.split(";");
+  let username = splitColons[1].split("=");
+  let password = splitColons[2].split("=");
+  let database = splitColons[3].split(" = ");
+  var dbConfig = {
+    user: username[1],
+    password: password[1],
+    server: "58.65.129.101",
+    database: database[1],
+  };
+  console.log(database[1])
+  var today = new Date();
+  today = today.toISOString().replace('T',' ').replace('Z','')
+    console.log(today)
+  var Query ="update cmatrix_received_sms set crsms_is_read = 1 where crsms_is_read = 0 and crsms_from_user_key = @FromUser and crsms_to_user_key = @ToUser";
+  var connection = new sql.ConnectionPool(dbConfig, function (err) {
+    var r = new sql.Request(connection);
+    r.input("ToUser", sql.VarChar, req.params.To),
+      r.input("FromUser", sql.VarChar, req.params.from),
+    r.multiple = true;
+    r.query(Query, function (err, recordsets) {
+      connection.close();
+      if (err) console.error(err);
+      else console.dir("SUCCESS", recordsets);
+    });
+  });
+})
+
 function CreateDbConnection(sqlConfig, callback) {
   let connectionFunc = null;
   if (sql.ConnectionPool) connectionFunc = sql.ConnectionPool;
@@ -57,4 +128,5 @@ function CreateDbConnection(sqlConfig, callback) {
     }
   });
 }
+
 module.exports = api;
